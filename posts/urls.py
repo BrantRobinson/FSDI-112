b5e33b63757af7be70cdf3747d5e0193a592ed18
BrantRobinson <brant.robinson@sdgku.edu>
2025-12-01 20:04:20 -0800

    class 3 updates

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostDeleteView, PostUpdateView
+from .views import PostListView, PostDetailView, PostCreateView, PostDeleteView, PostUpdateView, PostDraftListView, PostArchiveListView
 
 urlpatterns = [
     path("list/", PostListView.as_view(), name="post_list"),
+    path("draftlist/", PostDraftListView.as_view(), name="draft_list"),
+    path("archivelist/", PostArchiveListView.as_view(), name="archive_list"),
     path("", PostListView.as_view(), name="post_list"),
     path("detail/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
     path("new/", PostCreateView.as_view(), name="post_new"),
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,11 @@
 from .models import Post
-from django.contrib.auth.models import User
-from django.urls import reverse_lazy
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
+from django.views.generic.edit import FormMixin
+from .forms import CommentForm
+from django.urls import reverse_lazy, reverse
 from django.views.generic import (
     ListView,
     DetailView,
@@ -8,8 +13,6 @@ from django.views.generic import (
     UpdateView,
     DeleteView
 )
-
-
 
 class PostListView(ListView): 
     """
@@ -22,22 +25,66 @@ class PostListView(ListView):
     def get_queryset(self):
         user = self.request.user
 
-        published = Post.objects.filter(status__name="Published")
+        published = Post.objects.filter(status__name="Published").order_by("created_on").reverse()
 
         if user.is_authenticated:
-            own_posts = Post.objects.filter(author=user)
+            own_posts = Post.objects.filter(author=user).order_by("created_on").reverse()
             return (published | own_posts).distinct()
         return published
+    
+class PostDraftListView(LoginRequiredMixin, ListView):
+    template_name = "posts/draftlist.html"
+    model = Post
+    context_object_name = 'draft_posts'
 
+    def get_queryset(self):
+        user = self.request.user
+        drafts = Post.objects.filter(status__name="Draft", author = user).order_by("created_on").reverse()
+        return drafts
 
-class PostDetailView(DetailView): 
+class PostArchiveListView(LoginRequiredMixin, ListView):
+    template_name = "posts/archivelist.html"
+    model = Post
+    context_object_name = 'archive_posts'
+
+    def get_queryset(self):
+        user = self.request.user
+        drafts = Post.objects.filter(status__name="Archived", author = user).order_by("created_on").reverse()
+        return drafts
+
+class PostDetailView(LoginRequiredMixin, FormMixin, DetailView): 
     """
     PostListView retrieves one object from the posts table in the database
     """
     template_name = "posts/detail.html"
     model = Post
     context_object_name = "single_post"
+    form_class = CommentForm
 
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk':self.object.id})
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = self.object.comments.all().order_by('-created_on')
+        
+        if 'form' not in context:
+            context['form'] = self.get_form() 
+        
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = self.object
+            comment.author = request.user
+            comment.save()
+            return super().form_valid(form)
+        else:
+            return super().form_invalid(form)
+    
 class PostCreateView(CreateView): 
     """
     Render a from with the specified fields to create a new post object 
@@ -50,7 +97,7 @@ class PostCreateView(CreateView):
         form.instance.author = self.request.user 
         return super().form_valid(form)
 
-class PostUpdateView(UpdateView): 
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView): 
     """
     Render a form that will allow the user to update certain fields of their post
     """
@@ -59,7 +106,15 @@ class PostUpdateView(UpdateView):
     model = Post
     fields = ["title", "subtitle", "body", "image", "status"]
 
-class PostDeleteView(DeleteView):
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+        
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
     """
     View used to delete a post
     """
